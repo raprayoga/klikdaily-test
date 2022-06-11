dbcdcca4fae842aff73cdea83a0d92a2b3e4fe42
Rafie Achmad Prayoga <prayogarafie@gmail.com>
2022-06-11 14:27:22 +0700

    install deppendency

---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Products from 'components/Products'
+import DetailOrder from 'components/DetailOrder'
+
+export default function Main() {
+  return (
+    <>
+      <DetailOrder />
+      <Products />
+    </>
+  )
+}
